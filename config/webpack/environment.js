526aa18174ef536e357427f55fc161d84e31af19
kiraha <kiraha@kiraha-no-MacBook-Pro.local>
2020-05-13 19:38:02 +0900

    Error message 日本語化

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,10 +1,13 @@
 const { environment } = require('@rails/webpacker')
-environment.plugins.append(
-    'Provide',
-    new webpack.ProvidePlugin({
-      $: 'jquery',
-      jQuery: 'jquery',
-      Popper: ['popper.js', 'default']
-    })
-  )
+
+const webpack = require('webpack')
+environment.plugins.prepend('Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery',
+    jQuery: 'jquery',
+    Popper: ['popper.js', 'default'],
+    noUiSlider: 'nouislider'
+  })
+)
+
 module.exports = environment
